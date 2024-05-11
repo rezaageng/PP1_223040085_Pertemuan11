3942ed5244a995701def4e4fcd3b21fdb19b27cd
rezaa <rezaageng5@gmail.com>
2024-05-11 18:17:58 +0700

    feat: add comments

--- src/ParsePost.java
+++ src/ParsePost.java
@@ -6,22 +6,25 @@ public class ParsePost {
         this.input = input;
     }
 
+    // method untuk melakukan parsing
     public int doParse() {
         stack = new Stack(20);
         char ch;
         int i;
         int num1, num2, interAns;
 
+        // looping melalui setiap karakter dalam input
         for (i = 0; i < input.length(); i++) {
-            ch = input.charAt(i);
-            stack.displayStack("" + ch + " ");
+            ch = input.charAt(i); // mengambil karakter pada index ke-i
+            stack.displayStack(ch + " "); // menampilkan stack
 
+            // jika karakter adalah angka
             if (ch >= '0' && ch <= '9') {
-                System.out.println("number");
-                stack.push((int) (ch - '0'));
+                stack.push((int) (ch - '0')); // push angka ke stack
             } else {
-                num2 = stack.pop();
-                num1 = stack.pop();
+                num2 = stack.pop(); // mengambil dan menghapus elemen teratas dari stack, disimpan ke num2
+                num1 = stack.pop(); // mengambil dan menghapus elemen teratas dari stack, disimpan ke num1
+                // melakukan operasi sesuai dengan karakter
                 interAns = switch (ch) {
                     case '+' -> num1 + num2;
                     case '-' -> num1 - num2;
@@ -29,11 +32,11 @@ public class ParsePost {
                     case '/' -> num1 / num2;
                     default -> 0;
                 };
-                stack.push(interAns);
+                stack.push(interAns); // push hasil operasi ke stack
             }
         }
 
-        interAns = stack.pop();
-        return interAns;
+        interAns = stack.pop(); // mengambil dan menghapus elemen teratas dari stack, disimpan ke interAns
+        return interAns; // mengembalikan interAns sebagai hasil
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -3,16 +3,19 @@ public class Stack extends AbstractStack {
         super(capacity);
     }
 
+    // method untuk menampilkan stack
     public void displayStack(String s) {
-        System.out.println(s);
+        System.out.println(s); // menapilkan string yang diberikan pada parameter
         System.out.println("stack (bottom ->): ");
 
+        // looping melalui setiap elemen dalam stack
         for (int i = 0; i < size(); i++) {
-            System.out.println(peek(i) + " ");
+            System.out.println(peek(i) + " "); // menampilkan elemen pada index ke-i
         }
     }
 
+    // method untuk mendapatkan elemen pada index ke-n
     public int peek(int n) {
-        return arr[n];
+        return arr[n]; // mengembalikan elemen pada index ke-n
     }
 }
